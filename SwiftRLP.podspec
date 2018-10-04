23dfab262fba842ad839b3c6e33e1b0dab0497ba
Alex Vlasov <alex.m.vlasov@gmail.com>
2018-10-04 16:05:58 +0300

    move tests

--- SwiftRLP.podspec
+++ SwiftRLP.podspec
@@ -10,15 +10,15 @@ DESC
 s.homepage         = "https://github.com/shamatar/SwiftRLP"
 s.license          = 'Apache License 2.0'
 s.author           = { "Alex Vlasov" => "alex.m.vlasov@gmail.com" }
-s.source           = { :git => 'https://github.com/matterinc/web3swift.git', :tag => s.version.to_s }
+s.source           = { :git => 'https://github.com/shamatar/SwiftRLP.git', :tag => s.version.to_s }
 s.social_media_url = 'https://twitter.com/shamatar'
 
 s.swift_version = '4.1'
 s.module_name = 'SwiftRLP'
 s.ios.deployment_target = "9.0"
 s.osx.deployment_target = "10.11"
-s.source_files = "Classes/*.{h,swift}",
-s.public_header_files = "SwiftRLP/*.{h}"
+s.source_files = "Classes/RLP.swift",
+s.public_header_files = "SwiftRLP/SwiftRLP.h"
 s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES' }
 
 s.dependency 'BigInt', '~> 3.1'
--- SwiftRLPTests/SwiftRLPTests.swift
+++ SwiftRLPTests/SwiftRLPTests.swift
@@ -7,28 +7,141 @@
 //
 
 import XCTest
+import BigInt
 @testable import SwiftRLP
 
 class SwiftRLPTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    func testRLPencodeShortString() {
+        let testString = "dog"
+        let encoded = RLP.encode(testString)
+        var expected = Data([UInt8(0x83)])
+        expected.append(testString.data(using: .ascii)!)
+        XCTAssert(encoded == expected, "Failed to RLP encode short string")
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testRLPencodeListOfShortStrings() {
+        let testInput = ["cat","dog"]
+        let encoded = RLP.encode(testInput as [AnyObject])
+        var expected = Data()
+        expected.append(Data([UInt8(0xc8)]))
+        expected.append(Data([UInt8(0x83)]))
+        expected.append("cat".data(using: .ascii)!)
+        expected.append(Data([UInt8(0x83)]))
+        expected.append("dog".data(using: .ascii)!)
+        XCTAssert(encoded == expected, "Failed to RLP encode list of short strings")
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testRLPdecodeListOfShortStrings() {
+        let testInput = ["cat","dog"]
+        var expected = Data()
+        expected.append(Data([UInt8(0xc8)]))
+        expected.append(Data([UInt8(0x83)]))
+        expected.append("cat".data(using: .ascii)!)
+        expected.append(Data([UInt8(0x83)]))
+        expected.append("dog".data(using: .ascii)!)
+        var result = RLP.decode(expected)!
+        XCTAssert(result.isList, "Failed to RLP decode list of short strings") // we got something non-empty
+        XCTAssert(result.count == 1, "Failed to RLP decode list of short strings") // we got something non-empty
+        result = result[0]!
+        XCTAssert(result.isList, "Failed to RLP decode list of short strings") // we got something non-empty
+        XCTAssert(result.count == 2, "Failed to RLP decode list of short strings") // we got something non-empty
+        XCTAssert(result[0]!.data == testInput[0].data(using: .ascii), "Failed to RLP decode list of short strings")
+        XCTAssert(result[1]!.data == testInput[1].data(using: .ascii), "Failed to RLP decode list of short strings")
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testRLPencodeLongString() {
+        let testInput = "Lorem ipsum dolor sit amet, consectetur adipisicing elit"
+        let encoded = RLP.encode(testInput)
+        var expected = Data()
+        expected.append(Data([UInt8(0xb8)]))
+        expected.append(Data([UInt8(0x38)]))
+        expected.append("Lorem ipsum dolor sit amet, consectetur adipisicing elit".data(using: .ascii)!)
+        XCTAssert(encoded == expected, "Failed to RLP encode long string")
+    }
+    
+    func testRLPdecodeLongString() {
+        let testInput = "Lorem ipsum dolor sit amet, consectetur adipisicing elit"
+        var expected = Data()
+        expected.append(Data([UInt8(0xb8)]))
+        expected.append(Data([UInt8(0x38)]))
+        expected.append(testInput.data(using: .ascii)!)
+        let result = RLP.decode(expected)!
+        XCTAssert(result.count == 1, "Failed to RLP decode long string")
+        XCTAssert(result[0]!.data == testInput.data(using: .ascii), "Failed to RLP decode long string")
+    }
+    
+    func testRLPencodeEmptyString() {
+        let testInput = ""
+        let encoded = RLP.encode(testInput)
+        var expected = Data()
+        expected.append(Data([UInt8(0x80)]))
+        XCTAssert(encoded == expected, "Failed to RLP encode empty string")
+    }
+    
+    func testRLPdecodeEmptyString() {
+        let testInput = ""
+        var expected = Data()
+        expected.append(Data([UInt8(0x80)]))
+        let result = RLP.decode(expected)!
+        XCTAssert(result.count == 1, "Failed to RLP decode empty string")
+        XCTAssert(result[0]!.data == testInput.data(using: .ascii), "Failed to RLP decode empty string")
+    }
+    
+    func testRLPencodeEmptyArray() {
+        let testInput = [Data]() as [AnyObject]
+        let encoded = RLP.encode(testInput)
+        var expected = Data()
+        expected.append(Data([UInt8(0xc0)]))
+        XCTAssert(encoded == expected, "Failed to RLP encode empty array")
+    }
+    
+    func testRLPdecodeEmptyArray() {
+        //        let testInput = [Data]()
+        var expected = Data()
+        expected.append(Data([UInt8(0xc0)]))
+        var result = RLP.decode(expected)!
+        XCTAssert(result.count == 1, "Failed to RLP decode empty array")
+        result = result[0]!
+        guard case .noItem = result.content else {return XCTFail()}
+    }
+    
+    func testRLPencodeShortInt() {
+        let testInput = 15
+        let encoded = RLP.encode(testInput)
+        let expected = Data([UInt8(0x0f)])
+        XCTAssert(encoded == expected, "Failed to RLP encode short int")
+    }
+    
+    func testRLPdecodeShortInt() {
+        let testInput = 15
+        let expected = Data([UInt8(0x0f)])
+        let result = RLP.decode(expected)!
+        
+        XCTAssert(result.count == 1, "Failed to RLP decode short int")
+        XCTAssert(BigUInt(result[0]!.data!) == testInput, "Failed to RLP decode short int")
+    }
+    
+    func testRLPencodeLargeInt() {
+        let testInput = 1024
+        let encoded = RLP.encode(testInput)
+        var expected = Data()
+        expected.append(Data([UInt8(0x82)]))
+        expected.append(Data([UInt8(0x04)]))
+        expected.append(Data([UInt8(0x00)]))
+        XCTAssert(encoded == expected, "Failed to RLP encode large int")
+    }
+    
+    func testRLPdecodeLargeInt() {
+        let testInput = 1024
+        var expected = Data()
+        expected.append(Data([UInt8(0x82)]))
+        expected.append(Data([UInt8(0x04)]))
+        expected.append(Data([UInt8(0x00)]))
+        let result = RLP.decode(expected)!
+        
+        XCTAssert(result.count == 1, "Failed to RLP decode large int")
+        XCTAssert(BigUInt(result[0]!.data!) == testInput, "Failed to RLP decode large int")
     }
 
 }
